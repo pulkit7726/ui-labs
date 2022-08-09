ad6d023399276aff0889cb43fc8d83ff82d0e613
veerukumar11010 <veeranderp@wavelabs.ai>
2022-08-09 20:03:16 +0530

    Ui Labs Storybook intial checkins

---
+++ src/components/stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import ButtonMemo, { ButtonComponent } from '../Button';
+
+export default {
+  title: 'Button',
+  component: ButtonComponent
+};
+
+export const Basic = () => (
+    <ButtonMemo />
+)
+
+
