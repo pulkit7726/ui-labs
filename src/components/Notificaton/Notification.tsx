e968db5e568ed6480039ce3bd2af43b6a9037cb9
Megha Sahu <megha.sahu@wavelabs.ai>
2022-09-12 14:10:39 +0530

    notification transition

--- src/components/Notificaton/Notification.tsx
+++ src/components/Notificaton/Notification.tsx
@@ -1,64 +1,196 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import Alert from '@mui/material/Alert';
-import { AlertTitle } from '@mui/material';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import { AlertTitle, Box, IconButton, SliderThumb } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import Fade from "@mui/material/Fade";
+import Slide from "@mui/material/Slide";
+import Grow from "@mui/material/Grow";
+import {
+  SnackbarProvider,
+  VariantType,
+  useSnackbar,
+  SnackbarContent,
+} from "notistack";
 
-
-
-// const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-//     props,
-//     ref,
-// ) {
-//     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-// });
 type NotiFicationProps = {
+  color?: "error" | "info" | "success" | "warning";
+  variaent?: "filled" | "outlined" | "standard";
+  message?: string;
+  title?: string;
+  Basic?: boolean;
+  direction?: "down" | "left" | "right" | "up";
+  BasicMessage?: string;
+  autoHideDuration?: number;
+  Slide_Transition?: boolean;
+  Fade_Transition?: boolean;
+  Grow_Transition?: boolean;
+  position?: {
+    horizontal: "center" | "left" | "right";
+    vertical: "bottom" | "top";
+  };
+};
 
-    color?: 'error'
-    | 'info'
-    | 'success'
-    | 'warning',
-    variaent?: 'filled'
-    | 'outlined'
-    | 'standard',
-    message?: string,
-    title?: string,
-    iconShow?: boolean,
-    autoHideDuration?: number
+export default function Notification({
+  color,
+  variaent,
+  message,
+  Slide_Transition,
+  Fade_Transition,
+  Grow_Transition,
+  direction,
+  BasicMessage,
+  title,
+  Basic,
+  autoHideDuration,
+  position,
+}: NotiFicationProps) {
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  const [slide, setOpenSlide] = React.useState(false);
+  const [fade, setOpenFade] = React.useState(false);
+  const [grow, setOpenGrow] = React.useState(false);
+  const handleClick = () => {
+    setOpenSnackbar(true);
+  };
+  const handleSlideClick = () => {
+    setOpenSlide(true);
+  };
+  const handleFadeClick = () => {
+    setOpenFade(true);
+  };
+  const handleGrowClick = () => {
+    setOpenGrow(true);
+  };
 
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
+    setOpenSnackbar(false);
+    setOpenSlide(false);
+    setOpenFade(false);
+    setOpenGrow(false);
+  };
+  const SlideTransition = ({ ...props }: any) => {
+    return <Slide {...props} direction={direction} />;
+  };
 
-}
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+  //const { enqueueSnackbar } = useSnackbar();
 
-export default function Notification({ color, variaent, message, title, iconShow, autoHideDuration }: NotiFicationProps) {
-    const [open, setOpen] = React.useState(false);
+  //   const handleNotistack = () => {
+  //     enqueueSnackbar(`notistack`);
+  //   };
 
-    const handleClick = () => {
-        setOpen(true);
-    };
+  return (
+    <Stack spacing={2} sx={{ width: "50px" }}>
+      {Basic && (
+        <>
+          <Button variant="outlined" onClick={handleClick}>
+            Basic
+          </Button>
+          <Snackbar
+            open={openSnackbar}
+            message={BasicMessage || message}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            action={action}
+          />
+        </>
+      )}
+      {Slide_Transition && (
+        <>
+          <Button variant="outlined" onClick={handleSlideClick}>
+            Slide
+          </Button>
+          <Snackbar
+            open={slide}
+            message={message}
+            autoHideDuration={autoHideDuration}
+            TransitionComponent={SlideTransition}
+            onClose={handleClose}
+            action={action}
+          />
+        </>
+      )}
+      {Fade_Transition && (
+        <>
+          <Button variant="outlined" onClick={handleFadeClick}>
+            Fade
+          </Button>
+          <Snackbar
+            open={fade}
+            message={message}
+            autoHideDuration={autoHideDuration}
+            TransitionComponent={Fade}
+            onClose={handleClose}
+            action={action}
+          />
+        </>
+      )}
+      {Grow_Transition && (
+        <>
+          <Button variant="outlined" onClick={handleGrowClick}>
+            Grow
+          </Button>
+          <Snackbar
+            open={grow}
+            message={message}
+            autoHideDuration={autoHideDuration}
+            TransitionComponent={Grow}
+            onClose={handleClose}
+            action={action}
+          />
+        </>
+      )}
 
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpen(false);
-    };
-
-    return (
-        <Stack spacing={2} sx={{ width: '50px' }}>
-            <Button variant="outlined" onClick={handleClick}>
-                click
-            </Button>
-            <Snackbar open={open} autoHideDuration={autoHideDuration} onClose={handleClose}  >
-                <Alert onClose={handleClose} color={color} severity={color} variant={variaent} sx={{ width: '100%' }}>
-                    <AlertTitle> {title}</AlertTitle>
-                    {message}
-                </Alert>
-            </Snackbar>
-
-        </Stack>
-    );
+      {!Basic && !Slide_Transition && !Fade_Transition && (
+        <>
+          <Button variant="outlined" onClick={handleClick}>
+            click
+          </Button>
+          <Snackbar
+            onClose={handleClose}
+            open={openSnackbar}
+            autoHideDuration={autoHideDuration}
+            anchorOrigin={{
+              horizontal: position?.horizontal || "left",
+              vertical: position?.vertical || "bottom",
+            }}
+          >
+            <Alert
+              onClose={handleClose}
+              color={color}
+              severity={color}
+              variant={variaent}
+              sx={{ width: "100%" }}
+            >
+              <AlertTitle> {title}</AlertTitle>
+              {message}
+            </Alert>
+          </Snackbar>
+        </>
+      )}
+    </Stack>
+  );
 }
--- src/stories/Notification.stories.tsx
+++ src/stories/Notification.stories.tsx
@@ -4,39 +4,42 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Notification from "components/Notificaton/Notification";
 
 
+
 export default {
-    title: "Notification",
-    component: Notification,
-
-
-
+  title: "Notification",
+  component: Notification,
 } as ComponentMeta<typeof Notification>;
 
-
-
-
-
 const Template: ComponentStory<typeof Notification> = (args) => (
-    <Notification {...args} />
+  <Notification {...args} />
 );
 
-export const Type = Template.bind({});
-Type.args = {
-    color: "error",
-    iconShow: true
+export const Basic_Customized = Template.bind({});
+Basic_Customized.args = {
+  color: "error",
+  Basic: false,
+  message: "this is error message ",
+  BasicMessage: "this is basic snackbar",
+  title: "Error",
+  autoHideDuration: 3000,
+};
 
-}
 export const Variant = Template.bind({});
 Variant.args = {
-    variaent: "outlined"
-}
-export const Message = Template.bind({});
-Message.args = {
-    message: "this is success message"
-}
-export const Title = Template.bind({})
-Title.args = {
-    title: "Success",
-    message: "this is success message",
-    autoHideDuration: 3000
-}
+  variaent: "outlined",
+  message: "you can choose any type variant",
+};
+export const position = Template.bind({});
+position.args = {
+  position: { vertical: "bottom", horizontal: "left" },
+  message: "You can change the position ",
+};
+export const transition = Template.bind({});
+transition.args = {
+  Slide_Transition: true,
+  direction: "right",
+  Fade_Transition: true,
+  Grow_Transition: true,
+  autoHideDuration: 2000,
+  message: "you can choose transition of snackbar",
+};
