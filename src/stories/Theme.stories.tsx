4d9fdcc770237afb562ddf44147e72bb340dc659
Abhijit <abhijit.patra@wavelabs.ai>
2022-09-29 11:28:17 +0530

    added under ui

--- src/stories/Theme.stories.tsx
+++ src/stories/Theme.stories.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { SwitchModeButton } from 'components/Theme/SwitchModeButton';
 
 export default {
-  title: 'Theme',
-  component: SwitchModeButton
+	title: 'UI/Theme',
+	component: SwitchModeButton,
 };
 
-export const Basic = () => (
-  <SwitchModeButton />
-)
+export const Basic = () => <SwitchModeButton />;
