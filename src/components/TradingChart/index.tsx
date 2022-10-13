271f1a844928d52e288b32f5ad4984a3ca26368a
arvindrawat <arvind.rawat@wavelabs.ai>
2022-10-13 14:35:32 +0530

    install igniteui-react-core library

--- src/components/TradingChart/index.tsx
+++ src/components/TradingChart/index.tsx
@@ -1,6 +1,5 @@
-import React,{useState,useEffect} from 'react'
-import { IgrFinancialChart } from 'igniteui-react-charts';
-import { IgrFinancialChartModule } from 'igniteui-react-charts';
+import React, { useState, useEffect } from 'react';
+import { IgrFinancialChart, IgrFinancialChartModule } from 'igniteui-react-charts';
 import StocksHistory from './utils/StocksHistory';
 
 type TradingChartProps = {
@@ -10,34 +9,36 @@ type TradingChartProps = {
   yAxisTitle:string,
   chartType:string,
   thickness:any
-}
+};
 
 IgrFinancialChartModule.register();
 
-function TradingChart({chartTitle,subtitle,yAxisMode,yAxisTitle,chartType,thickness}:TradingChartProps) {
-
-  const [data,setData]=useState<any>([]);
-  useEffect(()=>{
+function TradingChart({
+  chartTitle, subtitle, yAxisMode, yAxisTitle, chartType, thickness,
+}:TradingChartProps) {
+  const [data, setData] = useState<any>([]);
+  useEffect(() => {
     StocksHistory.getMultipleStocks().then((stocks: any[]) => {
       setData(stocks);
     });
-  },[]);
-  
+  }, []);
+
   return (
-    <div className="container sample" >
-                <div className="container">
-                <IgrFinancialChart
-                  height='30rem'
-                    chartType={chartType}
-                    thickness={thickness}
-                    chartTitle={chartTitle}
-                    subtitle={subtitle}
-                    yAxisMode={yAxisMode}
-                    yAxisTitle={yAxisTitle}
-                    dataSource={data} />
-                </div>
-            </div>
-  )
+    <div className="container sample">
+      <div className="container">
+        <IgrFinancialChart
+          height="30rem"
+          chartType={chartType}
+          thickness={thickness}
+          chartTitle={chartTitle}
+          subtitle={subtitle}
+          yAxisMode={yAxisMode}
+          yAxisTitle={yAxisTitle}
+          dataSource={data}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default TradingChart
+export default TradingChart;
