a426f13dc4c3800c7351962491ebab61f4270896
Pulkit Srivastava <pulkit@nbos.vinisoft.com>
2022-10-19 09:35:12 +0530

    resizable grid

--- src/components/ResizableGrid/__test__/ResizableGrid.test.tsx
+++ src/components/ResizableGrid/__test__/ResizableGrid.test.tsx
@@ -6,23 +6,19 @@ import ResizableGrid from '../index';
 import * as ResizableGridStories from 'stories/ResizableGrid.stories';
 import { data, dataLabel, card, imageData } from 'data/ResizableGrid.json';
 
-const {
-  ResizableGridWithTable,
-  ResizableGridWithImage,
-  MultipleElementWithGrid,
-} = composeStories(ResizableGridStories);
+const { WithTable, WithImage, MultipleElementWithGrid } =
+  composeStories(ResizableGridStories);
 
 test('should render as Resizable Grid With Table', () => {
   render(<ResizableGrid display={'table'} data={data} dataLabel={dataLabel} />);
-  expect(ResizableGridWithTable.args?.display === 'table').toBeTruthy();
-  expect(ResizableGridWithTable.args?.data?.length === 10).toBeTruthy();
-  expect(ResizableGridWithTable.args?.dataLabel?.name === 'Name').toBeTruthy();
+  expect(WithTable.args?.display === 'table').toBeTruthy();
+  expect(WithTable.args?.data?.length === 10).toBeTruthy();
 });
 
 test('should render as Resizable Grid With Image', () => {
   render(<ResizableGrid display={'table'} imageData={imageData} />);
-  expect(ResizableGridWithImage.args?.display === 'image').toBeTruthy();
-  expect(ResizableGridWithImage.args?.imageData?.length === 7).toBeTruthy();
+  expect(WithImage.args?.display === 'image').toBeTruthy();
+  expect(WithImage.args?.imageData?.length === 7).toBeTruthy();
 });
 
 test('should render as Multiple Element With Grid', () => {
@@ -33,7 +29,7 @@ test('should render as Multiple Element With Grid', () => {
       dataLabel={dataLabel}
       card={card}
       imageData={imageData}
-    />
+    />,
   );
   expect(MultipleElementWithGrid.args?.display === 'multiple').toBeTruthy();
   expect(MultipleElementWithGrid.args?.imageData?.length === 7).toBeTruthy();
